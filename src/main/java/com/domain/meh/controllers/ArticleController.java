b920678822818dcf983f6ba085076de3e093dd24
Jonathan Lindahl <Jonathan.Lindahl@humanit.se>
2020-10-19 20:32:48 +0200

    db config

--- src/main/java/com/domain/meh/controllers/ArticleController.java
+++ src/main/java/com/domain/meh/controllers/ArticleController.java
@@ -1,4 +1,20 @@
 package com.domain.meh.controllers;
 
+import com.domain.meh.models.Article;
+import com.domain.meh.services.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ArticleController {
+    @Autowired
+    ArticleService articleService;
+    
+    @GetMapping("/all")
+    public List<Article> getAll() {
+        return articleService.getAll();
+    }
 }
