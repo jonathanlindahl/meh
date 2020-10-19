22c5d09cda1f5f319f02e0ea416c04a9a06b9e04
Jonathan Lindahl <Jonathan.Lindahl@humanit.se>
2020-10-19 20:08:15 +0200

    first commit

---
+++ src/main/java/com/domain/meh/MehApplication.java
@@ -0,0 +1,13 @@
+package com.domain.meh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MehApplication.class, args);
+	}
+
+}
