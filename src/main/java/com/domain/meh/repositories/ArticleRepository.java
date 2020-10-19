1025d3a61bc4480aa64e973ea54aeeab6d3d3306
Jonathan Lindahl <Jonathan.Lindahl@humanit.se>
2020-10-19 20:26:59 +0200

    repo and service

--- src/main/java/com/domain/meh/repositories/ArticleRepository.java
+++ src/main/java/com/domain/meh/repositories/ArticleRepository.java
@@ -1,12 +1,10 @@
 package com.domain.meh.repositories;
 
 import com.domain.meh.models.Article;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface ArticleRepository extends CrudRepository<Article, Long> {
-    List<Article> findByTitle();
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+    Article findByTitle();
 }
--- src/main/java/com/domain/meh/services/ArticleService.java
+++ src/main/java/com/domain/meh/services/ArticleService.java
@@ -1,7 +1,28 @@
 package com.domain.meh.services;
 
+import com.domain.meh.models.Article;
+import com.domain.meh.repositories.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ArticleService {
+    @Autowired
+    ArticleRepository articleRepository;
+    
+    public List<Article> getAll() {
+        return articleRepository.findAll();
+    }
+    
+    public Optional<Article> getByPublished(@RequestParam Long published) {
+        return articleRepository.findById(published);
+    }
+    
+    public Article getByTitle(@RequestParam String title) {
+        return articleRepository.findByTitle();
+    }
 }
