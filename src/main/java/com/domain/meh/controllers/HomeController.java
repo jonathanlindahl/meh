82fc8c98fed0988ea6bc89895cdc198f5db49805
Jonathan Lindahl <Jonathan.Lindahl@humanit.se>
2020-10-30 11:38:40 +0100

    articles on index page

--- src/main/java/com/domain/meh/controllers/HomeController.java
+++ src/main/java/com/domain/meh/controllers/HomeController.java
@@ -1,12 +1,23 @@
 package com.domain.meh.controllers;
 
+import com.domain.meh.models.Article;
+import com.domain.meh.services.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+    @Autowired
+    ArticleService articleService;
+    
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<Article> articles = articleService.getAll();
+        model.addAttribute("allarticles", articles);
         return "index";
     }
 }
